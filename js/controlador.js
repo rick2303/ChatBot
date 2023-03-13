34b258f566550796635edfb77e1e7a30673ae0be
Gerardo2811 <gerarmontoya@yahoo.es>
2023-03-12 21:44:52 -0600

    Chatbot estilo burbujas de chat

--- js/controlador.js
+++ js/controlador.js
@@ -1,37 +1,49 @@
 
-// Inicia la conversación
-console.log("¡Hola! Soy un chatbot. ¿En qué puedo ayudarte?");
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+//variables para la hora actual
+var fechaActual = new Date();
+  var hora = fechaActual.getHours();
+  var minutos = fechaActual.getMinutes();
+  var segundos = fechaActual.getSeconds();
+  var horaActual = hora + ":" + minutos + ":" + segundos;
 
-// Mantiene la conversación hasta que el usuario diga adiós
-rl.on('line', (entrada) => {
-    const respuesta = generarRespuesta(entrada);
-    console.log(respuesta);
-});
-
-function generarRespuesta(entrada) {
-    let respuesta = "";
-    if (entrada.includes("hola")) {
-        respuesta = "¡Hola! ¿Cómo estás?";
-    } else if (entrada.includes("bien")) {
-        respuesta = "Me alegra escuchar eso.";
-    } else if (entrada.includes("mal")) {
-        respuesta = "Lo siento mucho. ¿Quieres hablar sobre lo que te preocupa?";
-    } else if (entrada.includes("chiste")) {
-        respuesta = "¿Por qué los programadores prefieren el frío? Porque les gusta abrir y cerrar paréntesis.";
-    } else if (entrada.includes("gracias")) {
-        respuesta = "De nada, ¡estoy aquí para ayudarte!";    
-    } else if (entrada.includes("adios")) {
-        respuesta = "¡Hasta luego!";
-        process.exit();
-    } else if (entrada.includes("personalidad")) {
-        if(entrada === "bien") {
-            respuesta = "creo que segun tus preguntas y respuestas eres bueno";
-        }
-    } 
-    return respuesta;
-}
+  function agregarIcono() {
+    var textarea = document.getElementById("textarea1");
+    var icono = '<i class="fas fa-smile"></i>';
+    textarea.innerHTML += icono;
+  }
+// Función que toma la entrada del usuario y devuelve una respuesta del chatbot
+function chatbot() {
+    // Define un objeto JSON con preguntas y respuestas predefinidas
+    let respuestas = {
+        "hola": "¡Hola! ¿En qué puedo ayudarte?",
+        "adios": "¡Hasta luego!",
+        "como estas": "Estoy bien, gracias por preguntar. ¿Y tú?",
+        "ayuda": "Claro, ¿en qué necesitas ayuda?",
+        "hora":"La hora actual es: "+ horaActual,
+        "tu nombre":"Mi nombre es Feelbot",
+        "como te llamas": "Me llamo Feelbot",
+        "estoy bien":"Me alegra, sigue asi",
+        "soy curioso":"Hmmmmmm... dejame ver....."
+    };
+    let valoracion=0;
+    let user= `<i class="fa-solid fa-user"></i>`;
+    let entrada = document.getElementById("texto1").value;
+    let salida2= document.getElementById("textarea1").innerHTML+=`<div id="user">${entrada} : ${user}</div>`;
+    let salida = document.getElementById("textarea1")
+    let bot= `<i class="fa-solid fa-robot"></i>`;
+    entrada = entrada.toLowerCase(); // Convertir la entrada del usuario a minúsculas para una búsqueda insensible a mayúsculas y minúsculas
+        
+        for (let clave in respuestas) {
+            if (entrada.indexOf(clave) !== -1) {
+                valoracion=valoracion+1;
+                return salida.innerHTML += `<div id="botcito">${bot} : ${respuestas[clave]}<br></div>`; // Devuelve la respuesta correspondiente si se encuentra una clave en la entrada del usuario
+                
+            }
+            if(valoracion>4 && entrada.indexOf(clave) !== -1){
+                return salida.innerHTML += `<div id="botcito">${bot} : Eres muy curioso <br></div>`
+            }
+            }
+            
+        return salida.innerHTML+=`<div id="botcito">${bot} : Lo siento, no entiendo lo que estás diciendo.<br></div>`; // Respuesta predeterminada si no se encuentra ninguna clave en la entrada del usuario
+        
+    }
